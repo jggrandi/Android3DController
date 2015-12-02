fd3fb81111f1c22eab29e9a90114638c632785bf
Jeronimo Grandi <jggrandi@gmail.com>
2015-12-02 16:12:40 -0200

    Android App Added
    It connects with a server via TCP protocol.
    It sends:
     * Rotation matrix (VolumeDown + device rotation)
     * Translation matrix (Touch and scroll)
     * Scale matrix (Two touches)

---
+++ app/src/main/java/br/ufrgs/inf/control/control/MainActivity.java
@@ -0,0 +1,288 @@
+package br.ufrgs.inf.control.control;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.opengl.Matrix;
+import android.os.AsyncTask;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+
+    private TextView text;
+    private SensorManager mSensorManager;
+    private TCPClient tcp;
+
+    private float[] rotationMatrix = new float[16];
+    private float[] rotationMatrixPrev = new float[16];
+    private float[] rotationMatrixStep = new float[16];
+
+    float[] translation = new float[4];
+    private float[] translationMatrix = new float[16];
+
+    private float[] touchPosition = new float[2];
+
+    private float[] scaleMatrix = new float[16];
+    private float scale = 1.f;
+
+    boolean translationActive;
+    boolean rotationActive;
+
+    private ScaleGestureDetector mScaleDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView)findViewById(R.id.text);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_UI);
+        new connectTask().execute("");
+
+        Matrix.setIdentityM(rotationMatrix, 0);
+        Matrix.setIdentityM(rotationMatrixPrev,0);
+        Matrix.setIdentityM(rotationMatrixStep,0);
+        Matrix.setIdentityM(translationMatrix,0);
+        Matrix.setIdentityM(scaleMatrix,0);
+        translation = new float[]{0,0,0,1};
+
+        mScaleDetector = new ScaleGestureDetector(this, new ScaleListener());
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1)
+    {
+        //Do nothing.
+    }
+
+    public float [] fixMatrix(float m[]){
+        Log.d("asd", matrix2String(m));
+        float r[] = m.clone();
+        r[0] = m[4];
+        r[1] = m[5];
+        r[2] = m[6];
+
+        r[4] = m[8];
+        r[5] = m[9];
+        r[6] = m[10];
+
+        r[8] = m[0];
+        r[9] = m[1];
+        r[10] = m[2];
+        Log.d("qwe", matrix2String(r));
+        return r;
+    }
+
+
+    public byte [] float2ByteArray (float matrix[])
+    {
+        ByteBuffer buffer = ByteBuffer.allocate(4 * matrix.length);
+        for(int i=0; i<matrix.length; i++) buffer.putFloat(matrix[i]);
+
+        byte r[] = buffer.array();
+        for(int i=0; i<matrix.length; i++) {
+            byte b = r[i*4 + 0];
+            r[i*4 + 0] = r[i*4 + 3];
+            r[i*4 + 3] = b;
+            b = r[i*4 + 1];
+            r[i*4 + 1] = r[i*4 + 2];
+            r[i*4 + 2] = b;
+        }
+        return r;
+    }
+
+    public String matrix2String(float m[]){
+        return String.format("%.2f %.2f %.2f %.2f \n%.2f %.2f %.2f %.2f \n%.2f %.2f %.2f %.2f \n%.2f %.2f %.2f %.2f \n",
+                m[0], m[4], m[8], m[12], m[1], m[5], m[9], m[13], m[2], m[6], m[10], m[14], m[3], m[7], m[11], m[15]);
+    }
+    int asd = 10;
+    public void sendData(){
+        text.setText("");
+
+        if(rotationActive) {
+            Matrix.invertM(rotationMatrixStep, 0, rotationMatrixPrev, 0);
+            Matrix.multiplyMM(rotationMatrixStep, 0, rotationMatrix, 0, rotationMatrixStep, 0);
+        }else{
+            Matrix.setIdentityM(rotationMatrixStep,0);
+        }
+
+        Matrix.setIdentityM(translationMatrix, 0);
+        translationMatrix[12] = translation[0] * -0.003f;
+        translationMatrix[13] = translation[1] * 0.003f;
+        translationMatrix[14] = translation[2] * 0.003f;
+        translation[0] = translation[1] = translation[2] = 0;
+
+
+        Matrix.setIdentityM(scaleMatrix, 0);
+        scale = (scale + 1.0f)/2.0f;
+        scaleMatrix[0] = scaleMatrix[5] = scaleMatrix[10] = scale;
+        scale = 1.0f;
+
+        text.append(matrix2String(rotationMatrixStep)+"\n");
+        text.append(matrix2String(translationMatrix)+"\n");
+        text.append(matrix2String(scaleMatrix)+"\n");
+
+        byte a[] = float2ByteArray(rotationMatrixStep);
+        byte b[] = float2ByteArray(translationMatrix);
+        byte c[] = float2ByteArray(scaleMatrix);
+        byte d[] = float2ByteArray(rotationMatrix);
+        byte[] packet = new byte[a.length + b.length + c.length];
+        System.arraycopy(a, 0, packet, 0, a.length);
+        System.arraycopy(b, 0, packet, a.length, b.length);
+        System.arraycopy(c, 0, packet, a.length + b.length, c.length);
+        //System.arraycopy(c, 0, packet, a.length + b.length + c.length, d.length);
+
+        try {
+            if(asd--<0){
+                tcp.sendBytes(packet);
+                if(tcp.socket == null || !tcp.socket.isConnected()){
+                    text.append("Conectando...\n");
+                }else{
+                    text.append("Conectado\n");
+                }
+                if(tcp.dos != null){
+                    text.append("Tamanho Saída TCP: "+tcp.dos.size()+"\n");
+                }
+            }
+        }catch (IOException e) {
+
+        }
+        rotationMatrixPrev = rotationMatrix.clone();
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) return;
+
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values);
+        Matrix.invertM(rotationMatrix, 0, rotationMatrix, 0);
+
+        //rotationMatrix = fixMatrix(rotationMatrix);
+        sendData();
+    }
+
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+
+        if(keyCode == KeyEvent.KEYCODE_VOLUME_UP){
+
+        }
+
+        if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            switch (action) {
+                case KeyEvent.ACTION_DOWN:
+                    rotationMatrixPrev = rotationMatrix.clone();
+                    rotationActive = true;
+                    return true;
+                case KeyEvent.ACTION_UP:
+                    rotationActive = false;
+                    return true;
+            }
+        }
+        return super.dispatchKeyEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        mScaleDetector.onTouchEvent(event);
+        if(mScaleDetector.isInProgress()) return true;
+
+        int action = MotionEventCompat.getActionMasked(event);
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN) :
+
+                touchPosition[0] = event.getX();
+                touchPosition[1] = event.getY();
+                translation[0] = translation[1] = translation[2] = 0;
+                translationActive = true;
+
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                if(event.getPointerCount() > 1) {
+                    touchPosition[0] = event.getX();
+                    touchPosition[1] = event.getY();
+                    translationActive = false;
+                    return true;
+                }
+                if(translationActive){
+                    float[] t = new float[]{event.getX()-touchPosition[0],event.getY()-touchPosition[1],0,1};
+                    Matrix.multiplyMV(t,0,rotationMatrix,0,t,0);
+                    translation[0] += t[0];
+                    translation[1] += t[1];
+                    translation[2] += t[2];
+                    touchPosition[0] = event.getX();
+                    touchPosition[1] = event.getY();
+                }
+                return true;
+            case (MotionEvent.ACTION_UP) :
+                translationActive = false;
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
+    }
+
+    public class connectTask extends AsyncTask<String,String,TCPClient> {
+
+        @Override
+        protected TCPClient doInBackground(String... message) {
+
+            //we create a TCPClient object and
+            tcp = new TCPClient(new TCPClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+            tcp.run();
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+            //in the arrayList we add the messaged received from server
+            Log.d(values[0], values[0]);
+            // notify the adapter that the data set has changed. This means that new message received
+            // from server was added to the list
+        }
+    }
+
+
+
+
+    private class ScaleListener
+            extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            scale *= detector.getScaleFactor();
+
+
+            //invalidate();
+            return true;
+        }
+    }
+
+}
---
+++ app/src/main/java/br/ufrgs/inf/control/control/TCPClient.java
@@ -0,0 +1,115 @@
+package br.ufrgs.inf.control.control;
+
+import android.util.Log;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TCPClient {
+
+    private String serverMessage;
+    public static final String SERVERIP = "143.54.13.40"; //your computer IP address
+    public static final int SERVERPORT = 8002;
+    private OnMessageReceived mMessageListener = null;
+    private boolean mRun = false;
+
+    BufferedReader in;
+
+    public OutputStream out;
+    public DataOutputStream dos;
+
+    public TCPClient(OnMessageReceived listener) {
+        mMessageListener = listener;
+    }
+
+   /* public void sendMessage(String message){
+        if (out != null && !out.checkError()) {
+            out.println(message);
+            out.flush();
+        }
+    }
+*/
+    public void sendBytes(byte[] myByteArray) throws IOException {
+        sendBytes(myByteArray, 0, myByteArray.length);
+    }
+
+    public void sendBytes(byte[] myByteArray, int start, int len) throws IOException {
+        if (len < 0)
+            throw new IllegalArgumentException("Negative length not allowed");
+        if (start < 0 || start >= myByteArray.length)
+            throw new IndexOutOfBoundsException("Out of bounds: " + start);
+        if(dos == null || out == null) return;
+        //dos.writeInt(len);
+        if (len > 0) {
+            dos.write(myByteArray, start, len);
+        }
+    }
+    public Socket socket;
+    public void stopClient(){
+        mRun = false;
+    }
+
+    public void run() {
+
+        mRun = true;
+
+        try {
+            //here you must put your computer's IP address.
+            InetAddress serverAddr = InetAddress.getByName(SERVERIP);
+
+            Log.e("TCP Client", "C: Connecting...");
+
+            //create a socket to make the connection with the server
+            socket = new Socket(serverAddr, SERVERPORT);
+
+            try {
+
+                //send the message to the server
+                out = socket.getOutputStream();
+                dos = new DataOutputStream(out);
+
+                Log.e("TCP Client", "C: Sent.");
+
+                Log.e("TCP Client", "C: Done.");
+
+                //receive the message which the server sends back
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                //in this while the client listens for the messages sent by the server
+                while (mRun) {
+                    serverMessage = in.readLine();
+
+                    if (serverMessage != null && mMessageListener != null) {
+                        //call the method messageReceived from MyActivity class
+                        mMessageListener.messageReceived(serverMessage);
+                    }
+                    serverMessage = null;
+
+                }
+
+                Log.e("RESPONSE FROM SERVER", "S: Received Message: '" + serverMessage + "'");
+
+            } catch (Exception e) {
+
+                Log.e("TCP", "S: Error", e);
+
+            } finally {
+                //the socket must be closed. It is not possible to reconnect to this socket
+                // after it is closed, which means a new socket instance has to be created.
+                socket.close();
+            }
+
+        } catch (Exception e) {
+
+            Log.e("TCP", "C: Error", e);
+
+        }
+
+    }
+
+    //Declare the interface. The method messageReceived(String message) will must be implemented in the MyActivity
+    //class at on asynckTask doInBackground
+    public interface OnMessageReceived {
+        public void messageReceived(String message);
+    }
+}
